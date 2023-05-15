c6a3e55fd7a587d71856825efd050a8b6a9b3e62
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-05-15 19:00:32 +0900

    [feature/#10-vts-process] VTS 세션 질문 정적 데이터 추가

--- src/constants/static.ts
+++ src/constants/static.ts
@@ -2,4 +2,7 @@ export default {
   what: ` VTS(Visual Thinking Strategies)는 인지심리학자 하우젠(Abigail Housen)과 MoMA의
   미술관 에듀케이터 (Director of Education)였던 예나윈(Philip Yenawine)에 의해 개발된 질문 기반
   교수법입니다.`,
+  first: "이 그림에서 무슨 일이 일어나고 있나요?",
+  second: "무엇을 보고 그렇게 말했나요?",
+  third: "또 무엇을 더 찾을 수 있나요?",
 };
