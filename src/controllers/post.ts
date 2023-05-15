3e158cf69e460cdb5f0a4306086668ca0290bc3a
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-05-15 19:01:21 +0900

    [feature/#10-vts-process] 각 단계에 따라 쿼리 스트링을 통해 2번씩 호출하도록 로직 수정

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -13,27 +13,33 @@ import VTS from "~/constants/static";
  * @param next {NextFunction} error handling
  */
 
+interface IData {
+  user: string;
+  header: { additional?: boolean; end?: boolean; qna?: boolean };
+}
+
 export const postSessionGreeting = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const session = req.session as UserSession;
     const UserSessionId = `sess:${req.sessionID}`;
-    const { question, header } = req.body;
-    const { free } = header;
+    const user = `As visual thinking strategy practitioner,${req.body.user}`;
     const currentStage = "/session/greeting";
-    const nextStage = "/vts/init";
-    const result: AxiosResponse = await axios.post(`${FLASK_SERVER}/talk`, { question, header });
+    const nextStage = `/vts/init?additional=true`;
+    const data: IData = { user, header: { additional: false } };
 
-    const answer = result.data.contents.answer;
+    const result: AxiosResponse = await axios.post(`${FLASK_SERVER}/talk`, data);
+
+    const agent = result.data.contents.answer;
     const source = result.data.contents.source;
     const relevantSources = JSON.stringify(result.data.contents.relevantSources);
     await Message.create({
-      question,
-      answer,
+      user,
+      agent,
       source,
       relevantSources,
+      free: false,
       stage: currentStage,
       type: "dynamic",
-      free,
       UserSessionId,
     });
 
@@ -42,72 +48,134 @@ export const postSessionGreeting = async (req: Request, res: Response, next: Nex
 
     return res
       .status(200)
-      .json({ message: "greeting success", question, answer, source, relevantSources, currentStage, nextStage });
+      .json({ message: "greeting success", user, agent, source, relevantSources, currentStage, nextStage });
   } catch (e) {
     next(e);
   }
 };
+
+/**
+ * VTS Introduce and VTS agree
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
 
 export const postVTSInit = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const session = req.session as UserSession;
     const UserSessionId = `sess:${req.sessionID}`;
-    const { question, header } = req.body;
-    const { free } = header;
-    const currentStage = "/vts/init";
-    const nextStage = "/vts/first?additional=true";
-    const answer = VTS.what;
-
-    await Message.create({
-      question,
-      answer,
-      source: "static",
-      relevantSources: "static",
-      stage: currentStage,
-      type: "static",
-      free,
-      UserSessionId,
-    });
+    const { user } = req.body;
+    let additional;
+    if (req.query.additional === "true") additional = true;
+    else if (req.query.additional === "false") additional = false;
+    if (additional === undefined) return res.status(400).json({ message: "incorrect query string" });
+    if (!user) return res.status(400).json({ message: "incorrect data" });
+    const currentStage = `/vts/init?additional=${additional}`;
+    const nextStage = additional ? `/vts/init?additional=${!additional}` : `/vts/first?additional=${!additional}`;
 
     session.user.currentStage = currentStage;
     session.user.nextStage = nextStage;
 
-    return res.status(200).json({
-      message: "greeting success",
-      question,
-      answer,
-      source: "static",
-      relevantSources: "static",
-      currentStage,
-      nextStage,
-    });
+    if (additional) {
+      const agent = VTS.what;
+      await Message.create({
+        user,
+        agent,
+        source: "static",
+        free: false,
+        relevantSources: "static",
+        stage: currentStage,
+        type: "static",
+        UserSessionId,
+      });
+      return res.status(200).json({
+        message: "vts introduce success, please agree with starting to vts session",
+        user,
+        agent,
+        source: "static",
+        relevantSources: "static",
+        currentStage,
+        nextStage,
+      });
+    } else {
+      /**
+        TODO : 자연어 처리 로직 추가 
+      */
+      // const result: AxiosResponse = await axios.post(`${FLASK_SERVER}/talk`, data);
+      const agent = "thank you";
+      const source = "static";
+      const relevantSources = "static";
+      await Message.create({
+        user,
+        agent,
+        source,
+        free: false,
+        relevantSources,
+        stage: currentStage,
+        type: "dynamic",
+        UserSessionId,
+      });
+      return res.status(200).json({
+        message: "vts first init, reply for first vts question",
+        user,
+        agent,
+        first: VTS.first,
+        source: "static",
+        relevantSources: "static",
+        currentStage,
+        nextStage,
+      });
+    }
   } catch (e) {
     next(e);
   }
 };
 
+/**
+ * VTS first session and first additional quesiton
+ * TODO : 그림 정보를 알아내는 자연어 처리 로직 필요
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
+
 export const postVTSFirst = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const session = req.session as UserSession;
     const UserSessionId = `sess:${req.sessionID}`;
-    const { question, header } = req.body;
-    const { additional } = req.query;
-    const { free } = header;
-    const currentStage = "/vts/first";
-    const nextStage = "/vts/first?additional=false";
-    const result: AxiosResponse = await axios.post(`${FLASK_SERVER}/talk`, { question, header });
+    let { user } = req.body;
+    let additional;
+    if (req.query.additional === "true") additional = true;
+    else if (req.query.additional === "false") additional = false;
+    if (additional === undefined) return res.status(400).json({ message: "incorrect query string" });
+    if (!user) return res.status(400).json({ message: "incorrect data" });
+    const currentStage = `/vts/first?additional=${additional}`;
+    const nextStage = additional ? `/vts/first?additional=${!additional}` : `/vts/second?additional=${!additional}`;
 
-    const answer = result.data.contents.answer;
+    if (additional) {
+      user = `As visual thinking strategy practitioner Rephrase the question below and answer it. And create one additional question for it. \n ${user}`;
+    } else {
+      user = `As visual thinking strategy practitioner Rephrase the question below and answer it. \n ${user} `;
+    }
+    const data: IData = { user, header: { additional } };
+
+    const result: AxiosResponse = await axios.post(`${FLASK_SERVER}/talk`, data);
+
+    const agent = result.data.contents.answer;
     const source = result.data.contents.source;
     const relevantSources = JSON.stringify(result.data.contents.relevantSources);
+
     await Message.create({
-      question,
-      answer,
+      user,
+      agent,
       source,
+      free: false,
       relevantSources,
       stage: currentStage,
       type: "dynamic",
-      free,
       UserSessionId,
     });
 
@@ -115,11 +183,11 @@ export const postVTSFirst = async (req: Request, res: Response, next: NextFuncti
     session.user.nextStage = nextStage;
 
     return res.status(200).json({
-      message: "greeting success",
-      question,
-      answer,
-      source: "static",
-      relevantSources: "static",
+      message: additional ? "continue additional question" : "additional question end",
+      user,
+      agent,
+      source,
+      relevantSources,
       currentStage,
       nextStage,
     });
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { postSessionGreeting, postVTSInit } from "~/controllers";
+import { postSessionGreeting, postVTSInit, postVTSFirst } from "~/controllers";
 import { isSessionInit } from "~/lib/middlewares";
 
 const postRouter = express.Router();
 
 postRouter.post("/session/greeting", isSessionInit, postSessionGreeting);
 postRouter.post("/vts/init", isSessionInit, postVTSInit);
+postRouter.post("/vts/first", isSessionInit, postVTSFirst);
 
 export default postRouter;
