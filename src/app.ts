12c32e27a43e304aab7646137196a6752010a349
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-05-27 23:55:53 +0900

    [develop/second-proto] .

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(
     proxy: false, // 이게 있던 없던 nginx 에서 cookie 값에 대한 설정이 있다면 그게 덮어씌워짐.
     secret: "siwon",
     cookie: {
-      secure: false,
+      secure: true,
       httpOnly: false,
       domain: ".sgu-hci.p-e.kr",
       maxAge,
