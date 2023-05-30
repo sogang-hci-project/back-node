e99629f99d8edacad14d5274341bac9b1532b965
daeheon_macbook <neohgeek@gmail.com>
2023-05-31 08:56:11 +0900

    [fix/#22-message-modification] 프롬프트 추가 수정

--- src/constants/static.ts
+++ src/constants/static.ts
@@ -2,7 +2,7 @@ export default {
   introduce: `Greetings, my dear friend! Welcome to my painting Guernica.
   I, Pablo Picasso, extend my warmest salutations to you. With my brush as my voice and colors as my language, I endeavor to create a world where imagination knows no bounds.
   Now, can you introduce me about yourself? Maybe about your name or interest.`,
-  what: `Correct, Lets move on the questions. Questions harnesses the power of visual imagery to enhance understanding of the art.
+  what: `I see, Lets move on to the questions. Questions can harness the power of visual imagery to enhance understanding of the art.
   I'll ask you a couple of questions to help you find captivating features of my painting. Will you join me?`,
   first: "In the painting, what do you think is going on?",
   second: "What else can you find in the painting?",
