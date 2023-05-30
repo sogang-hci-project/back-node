f01d612089c1bd2a03f6e463acf25abe679e898c
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-05-30 16:58:49 +0900

    [feature/#17-session-auth] session cookie 를 사용하지 않는 방식으로 변경

--- src/routes/get.ts
+++ src/routes/get.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import { getInitSession } from "~/controllers";
+import { get } from "http";
+import { getInitSession, getPreInitSession } from "~/controllers";
+import { addSession } from "~/lib/middlewares";
 
 const getRouter = express.Router();
 
 getRouter.get("/session/init", getInitSession);
+getRouter.get("/session/pre", addSession, getPreInitSession);
 
 export default getRouter;
