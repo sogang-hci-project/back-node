bb434ab47d22e294a044358f8b4c9c4e2b1d500e
Siwon Jeon <87080940+siwonblue@users.noreply.github.com>
2023-06-06 20:22:34 +0900

    [develop/#cicd] code deploy hooks 별로scripts 파일 분리

---
+++ scripts/AfterInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu/src/back-node
+npm i
+sudo npm run build
--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/src/back-node
-npm i
-sudo npm run build
 sudo npm run kill:pm2
 sudo npm run start:pm2
